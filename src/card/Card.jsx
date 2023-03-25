afb36fa229a989ff6d42ca60260662f9ef70a8c6
timcrocodile <timcrocodile@proton.me>
2023-03-25 11:16:57 +0100

    cocktail saturday

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,5 +1,8 @@
 import styles from "./index.module.scss";
 import { useState } from "react";
+import { FaCocktail } from "react-icons/fa";
+import { ImBooks } from "react-icons/im";
+import { AiFillTag } from "react-icons/ai";
 
 const Card = ({ obj, setCardClick }) => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -20,21 +23,37 @@ const Card = ({ obj, setCardClick }) => {
     setIsHidden(!isHidden);
   };
 
+  const onMessageLike = () => {
+    setNLikes((prev) => prev + 1);
+    setLiked(true);
+  };
+
+  const [nLikes, setNLikes] = useState(0);
+  const [liked, setLiked] = useState(false);
+  // const handleDisappearText = () => {
+  //   setIsHidden(false);
+  // };
+
   return (
     <div className={styles.Card}>
-      <div className={styles.Headings}>
-        <h4 className={styles.Title}>{obj.strDrink}</h4>
-        <button onClick={onHandleClick} className={styles.Infos}>
-          +INFOs
-        </button>
-      </div>
-
       <img
         src={obj.strDrinkThumb}
         alt={obj.strCategory}
         onMouseOver={handleImageClick}
-      />
-
+        // onMouseOut={handleDisappearText}
+      />{" "}
+      <div className={styles.Headings}>
+        <h4 className={styles.Title}>{obj.strDrink}</h4>
+        <div>
+          <img
+            className={`heart ${liked ? "liked" : ""}`}
+            onClick={onMessageLike}
+            src="https://img.icons8.com/ios-glyphs/256/hearts.png"
+            alt="heart"
+          />
+          <span className="like-count">{nLikes}</span>
+        </div>
+      </div>
       {/* { <div className="Card__Text"> }
       {<div className={`Card__Text ${isHidden ? "hidden" : ""}`}> } */}
       {/* <div className={`Card__Text ${isHidden ? "" : "Card__Text--hidden"}`}> */}
@@ -43,25 +62,58 @@ const Card = ({ obj, setCardClick }) => {
           isHidden ? "" : styles.CardTexthidden
         }`}
       >
-        <h3 className={styles.DrinkName}>{obj.strDrink} 🍹 </h3>
-        <h3>Ingredienti :</h3>
+        {/* <div className={styles.CardTexthidden} id={isHidden}> */}
         <ul>
+          <li>
+            <h3 className={styles.DrinkName}>
+              {" "}
+              Type <FaCocktail /> {obj.strAlcoholic}{" "}
+            </h3>
+          </li>
+          <li>
+            {" "}
+            <h3>
+              {" "}
+              #tags
+              <AiFillTag /> {obj.strTags}
+            </h3>
+          </li>
+          <li>
+            <h3>
+              IBA name
+              <ImBooks />
+              {obj.strIBA}
+            </h3>
+          </li>
+          <li>
+            {/* <button onClick={onHandleClick} className={styles.Infos}>
+              Ecco la Ricetta!
+            </button> */}
+            <a onClick={onHandleClick} className={styles.Infos}>
+              VEDI LA RICETTA!
+            </a>
+          </li>
+        </ul>
+
+        {/* <h3>Ingredienti :</h3> */}
+        {/* <ul>
           <li>{obj.strIngredient1}</li>
           <li>{obj.strIngredient2}</li>
           <li>{obj.strIngredient3}</li>
-        </ul>
-        <h3 className={styles.cardInstructions} onClick={handleClick}>
-          Come prepararlo :
-          {/* {isExpanded
+        </ul>{" "} */}
+
+        {/* <h3 className={styles.cardInstructions} onClick={handleClick}>
+          Come prepararlo : */}
+        {/* {isExpanded
             ? obj.strInstructionsIT
             : obj.strInstructionsIT.slice(0, 25) + "..."} */}
-          {/* ho aggiunto la condizione che in caso di condizione non presenti debba stampare un p perchè sennò si bloccava quando non trovava la stringa */}
-          {obj.strInstructionsIT
+        {/* ho aggiunto la condizione che in caso di condizione non presenti debba stampare un p perchè sennò si bloccava quando non trovava la stringa */}
+        {/* {obj.strInstructionsIT
             ? isExpanded
               ? obj.strInstructionsIT
               : obj.strInstructionsIT.slice(0, 25) + "..."
             : "Istruzioni non presenti"}
-        </h3>
+        </h3> */}
       </div>
     </div>
   );
